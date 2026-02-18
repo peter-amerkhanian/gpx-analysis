66c485edfe1763ef39f0de68f568b1bf9fb06d5b
Peter Amerkhanian <peteamer@gmail.com>
2026-02-18 10:34:12 -0800

    codex fixed bearing degree change bug

--- gpx_analysis/analytics.py
+++ gpx_analysis/analytics.py
@@ -27,7 +27,9 @@ def compute_step_metrics(df: pd.DataFrame) -> pd.DataFrame:
 
     frame["step_bearing"] = np.array([*bearing, np.nan])
     frame["step_bearing"] = frame["step_bearing"].replace({0: np.nan})
-    frame["step_turn"] = frame["step_bearing"].diff().abs()
+    #  minimum circular difference, always in [0, 180]
+    delta_bearing = frame["step_bearing"].diff()
+    frame["step_turn"] = ((delta_bearing + 180) % 360 - 180).abs()
     frame["step_dist_m"] = np.array([0, *step_meters])
     frame["step_dist_f"] = frame["step_dist_m"] * 3.28084
     frame["step_elevation_m"] = frame["elevation_m"].diff().fillna(0)
@@ -41,7 +43,7 @@ def detect_hazards(df: pd.DataFrame, rolling_window: int = 3) -> pd.DataFrame:
     frame["avg_step_grade"] = frame["step_grade"].rolling(rolling_window).mean()
     frame["avg_bearing_change"] = frame["step_turn"].rolling(rolling_window).mean()
     thresholds = {
-        "light_descent": -0.029,
+        "light_descent": -0.049,
         "steep_descent": -0.099,
         "ultra_steep_descent": -0.199,
         "turn": 24,
@@ -57,20 +59,20 @@ def detect_hazards(df: pd.DataFrame, rolling_window: int = 3) -> pd.DataFrame:
 
     coming_off_descent = (frame["step_grade"].shift(-1).round(2) < np.mean([thresholds["light_descent"], thresholds["steep_descent"]]))
     coming_off_steep_descent = (frame["step_grade"].shift(-1).round(2) < np.mean([thresholds["steep_descent"], thresholds["ultra_steep_descent"]]))
-
+    coming_off_ultra_steep_descent = (frame["step_grade"].shift(-1).round(2) < (thresholds["ultra_steep_descent"] * 1.1))
     hazards = {
-        "light_descent": on_descent | (frame["avg_step_grade"].round(2) < -0.08),
-        "steep_descent": on_steep_descent | (frame["avg_step_grade"].round(2) < -0.15),
-        "ultra_steep_descent": on_ultra_steep_descent | (frame["avg_step_grade"].round(2) < -0.2),
+        "light_descent": on_descent | coming_off_descent,
+        "steep_descent": on_steep_descent | coming_off_steep_descent,
+        "ultra_steep_descent": on_ultra_steep_descent | coming_off_ultra_steep_descent,
         "turn_on_descent": (
             (on_turn & coming_off_descent)
             | (on_turn & on_descent)
-            | ((frame["avg_bearing_change"].round() > 24) & (frame["avg_step_grade"].round(2) < -0.05))
+            | ((frame["avg_bearing_change"].round() > thresholds["turn"]) & (frame["avg_step_grade"].round(2) < thresholds["light_descent"]))
         ),
         "turn_on_steep_descent": (
             (on_turn & coming_off_steep_descent)
             | (on_turn & on_steep_descent)
-            | ((frame["avg_bearing_change"].round() > 24) & (frame["avg_step_grade"].round(2) < -0.1))
+            | ((frame["avg_bearing_change"].round() > thresholds["turn"]) & (frame["avg_step_grade"].round(2) < thresholds["steep_descent"]))
         ),
         "climb": on_climb,
         "steep_climb": on_steep_climb
